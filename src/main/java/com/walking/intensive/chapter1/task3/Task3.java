e602a4550270883101f0db296b7f1ad35d039e9b
dspnstn <daniil.espenstein@gmail.com>
2024-12-04 17:04:40 +0200

    убраны лишние скобки

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,6 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        return (year >= 1 && year % 4 == 0) && (year % 400 == 0 || year % 100 != 0);
+        return year >= 1 && year % 4 == 0 && (year % 400 == 0 || year % 100 != 0);
     }
 }
