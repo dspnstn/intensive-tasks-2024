2d93321862524ec4fa176e26656ed707f4cbc272
dspnstn <daniil.espenstein@gmail.com>
2025-01-09 19:22:48 +0200

    метод solveEquation

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,17 +25,57 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
+        double a = 1;
         double b = 0;
         double c = 0;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0) {
+            return c == 0 ? "Бесконечное множество решений." : "Количество решений: 0.";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0) {
+            return "Количество решений: 1." + calculateLinearEquation(b, c);
+        }
+
+        if (b == 0 && c == 0) {
+            return "Количество решений: 1. Корень: 0.0";
+        }
+
+        double discriminant = calculateDiscriminant(a, b, c);
+
+        return (discriminant > 0) ? "Количество решений: 2." + calculateTwoRoots(a, b, discriminant)
+                : discriminant == 0 ? "Количество решений: 1." + calculateOneRoot(a, b)
+                : "Количество решений: 0.";
+    }
+
+    static String calculateLinearEquation(double b, double c) {
+        return " Корень: " + (-c / b);
+    }
+
+    static double calculateDiscriminant(double a, double b, double c) {
+        return b * b - 4 * a * c;
+    }
+
+    static String calculateOneRoot(double a, double b) {
+        return " Корень: " + (-b / (2 * a));
+    }
+
+    static String calculateTwoRoots(double a, double b, double discriminant) {
+        double firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        String twoRoots = "";
+
+        if (firstRoot < secondRoot) {
+            twoRoots += firstRoot + ";" + secondRoot;
+        } else if (secondRoot < firstRoot) {
+            twoRoots += secondRoot + ";" + firstRoot;
+        }
+
+        return " Корни: " + twoRoots;
     }
 }
